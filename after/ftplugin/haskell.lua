f8b81b050c21e71be01acb8699e73b65799ba92e
aitouka21 <wscheungae@gmail.com>
2023-11-30 00:01:15 +0800

    nvim config

---
+++ after/ftplugin/haskell.lua
@@ -0,0 +1,21 @@
+vim.g.haskell_tools = {
+  hls = {
+    on_attach = require('helpers.lsp').on_attach,
+  }
+}
+
+local ht = require('haskell-tools')
+
+local bufnr = vim.api.nvim_get_current_buf()
+local def_opts = { noremap = true, silent = true, buffer = bufnr, }
+
+vim.keymap.set('n', '<space>ca', vim.lsp.codelens.run, opts)
+vim.keymap.set('n', '<space>hs', ht.hoogle.hoogle_signature, opts)
+vim.keymap.set('n', '<space>ea', ht.lsp.buf_eval_all, opts)
+vim.keymap.set('n', '<leader>rr', ht.repl.toggle, opts)
+vim.keymap.set('n', '<leader>rf', function()
+  ht.repl.toggle(vim.api.nvim_buf_get_name(0))
+end, def_opts)
+vim.keymap.set('n', '<leader>rq', ht.repl.quit, opts)
+
+ht.lsp.start()
--- init.lua
+++ init.lua
@@ -189,6 +189,8 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
 
+  { "nvim-tree/nvim-web-devicons", lazy = true },
+
   {
     'folke/trouble.nvim',
     dependencies = {
@@ -260,6 +262,22 @@ require('lazy').setup({
     config = function()
       require('colorizer').setup()
     end,
+  },
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {
+      on_attach = require('helpers.lsp').on_attach,
+    },
+  },
+  {
+    'dmmulroy/tsc.nvim',
+    opts = {}
+  },
+  {
+    'mrcjkb/haskell-tools.nvim',
+    version = '^3', -- Recommended
+    ft = { 'haskell', 'lhaskell', 'cabal', 'cabalproject' },
   },
   require 'kickstart.plugins.autoformat',
 }, {})
@@ -377,51 +395,6 @@ vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open float
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 
--- [[ Configure LSP ]]
---  This function gets run when an LSP connects to a particular buffer.
-local on_attach = function(_, bufnr)
-  -- NOTE: Remember that lua is a real programming language, and as such it is possible
-  -- to define small helper and utility functions so you don't have to repeat yourself
-  -- many times.
-  --
-  -- In this case, we create a function that lets us more easily define mappings specific
-  -- for LSP related items. It sets the mode, buffer and description for us each time.
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = 'LSP: ' .. desc
-    end
-
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-  end
-
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-  -- See `:help K` for why this keymap
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-  -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
-
-  -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format()
-  end, { desc = 'Format current buffer with LSP' })
-end
-
 -- Enable the following language servers
 --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
 --
@@ -431,12 +404,9 @@ end
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  hls = {
-    filetypes = { 'haskell', 'lhaskell', 'cabal' },
-  },
+  hls = {},
   rust_analyzer = {},
   sqlls = {},
-  tsserver = {},
   gopls = {},
   eslint = {},
   lua_ls = {
@@ -473,9 +443,17 @@ mason_lspconfig.setup {
 
 mason_lspconfig.setup_handlers {
   function(server_name)
+    if (server_name == 'tsserver') then
+      return
+    end
+
+    if (server_name == 'hls') then
+      return
+    end
+
     require('lspconfig')[server_name].setup {
       capabilities = capabilities,
-      on_attach = on_attach,
+      on_attach = require('helpers.lsp').on_attach,
       settings = servers[server_name],
       filetypes = (servers[server_name] or {}).filetypes,
     }
